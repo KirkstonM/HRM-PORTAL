4e756eb2d4b513a47466aa7e36c31b13392f8f09
Kirkston Melder <www.odkmmelder@gmail.com>
2025-02-15 05:15:15 +0530

    refactor: testing

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,25 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-dotenv.config();
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+dotenv.config()
 
 //mongodb connection
-mongoose.connect(process.env.MONGODB_URI, {})
-  .then(() => console.log("MONGO DATABASE CONNECTED"))
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log('MONGO DATABASE CONNECTED'))
 
 //PORT INITIALIZATION
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT:::: ${PORT}`)
-});
-
-
+})
